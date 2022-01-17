08bde631e35ffbdb4a4381922b7c506e3b4d21c3
Yoonjae <kkang47140@gmail.com>
2022-01-17 16:20:31 +0900

    Initial Commit

---
+++ osusume_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  osusume_Project
+//
+//  Created by Yoonjae lee on 2022/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ osusume_Project/osusume_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  osusume_ProjectApp.swift
+//  osusume_Project
+//
+//  Created by Yoonjae lee on 2022/01/17.
+//
+
+import SwiftUI
+
+@main
+struct osusume_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
